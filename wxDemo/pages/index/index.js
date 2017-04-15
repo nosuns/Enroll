6726f93df99474e4c54ff460a39ad88f190c867c
mapleqiqi <jennyqiu012@163.com>
2017-04-15 21:32:57 +0800

    首页数据

--- wxDemo/pages/index/index.js
+++ wxDemo/pages/index/index.js
@@ -1,16 +1,18 @@
-const app = getApp();
 const AV = require('../../libs/av-weapp-min');
+var util = require('../../utils/util.js')
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
   data: {
-    userInfo: {},
     enrolled:'0', 
     copyright:"Copyright © 2017 Nople Studio",
     tabs: ["我参加的聚会", "我发起的聚会","测试链接"],
     activeIndex: "0",
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    enrolledList: [],
+    createdList: [],
+    enrolledNumbers: 0
   },
 
   onShareAppMessage: function () {
@@ -24,6 +26,7 @@ Page({
   onLoad: function () {
     console.log('onLoad')
     var that = this;
+    // 获取屏幕宽度，计算tab位置
     wx.getSystemInfo({
         success: function(res) {
             that.setData({
@@ -31,16 +34,85 @@ Page({
             });
         }
     });
+
+    // 从Enroll表中查出用户参加的所有聚会
     var user = AV.User.current();
-    this.setData({
-      userInfo: user
-    })
+    var query = new AV.Query('Enroll');
+    query.equalTo('user', user);
+    query.include('campaign');
+    query.find().then(function(enrollList) {
+      console.log('-----------------------');
+      var tempEnrolledList = [];
+      enrollList.forEach(function(enroll, i, a) {
+        // 格式化活动时间
+        var dateList = [];
+        dateList = that.parseCampaignDate(enroll.get('campaign'));
+        console.log(dateList);
+        // 计算参加该活动的总人数
+        var numberQuery = new AV.Query('Enroll');
+        numberQuery.select('number');
+        numberQuery.equalTo('campaign', enroll.get('campaign'));
+        numberQuery.find().then(function(numbers) {
+          var totalNumber = 0;
+          numbers.forEach(function(number, i, a) {
+            totalNumber = totalNumber + number.get('number');
+          })
+          console.log('总人数:'+totalNumber);
+          // that.setData({enrolledNumbers:totalNumber})
+        })
+        // 是否为本人发起
+        var isOwner = 0;
+        if(enroll.get('campaign').get('createdBy').id == user.id) {
+          isOwner = 1;
+        }
+        tempEnrolledList[i] = {'name': enroll.get('campaign').get('name'),
+                               'startTime': dateList[1],
+                               'endTime': dateList[2],
+                               'allDay': dateList[0],
+                               'isOwner': isOwner,
+                               'totalNumber': enroll.totalNumber}
+        console.log(tempEnrolledList);
+      })
+
+      console.log('1----------------------');
+      that.setData({enrolledList: tempEnrolledList});
+      console.log('2----------------------');
+      console.log(that.data.enrolledList);
+    }).catch(console.error);
+
+
+    // 查出用户发起的所有聚会
+
+  },
+
+  // 格式化活动时间：根据开始日期与结束日期是否相等，来显示不同格式的活动时间
+  parseCampaignDate: function (campaign) {
+    var oneDay = '';
+    var startDateTime = '';
+    var endDateTime = '';
+    var startDate = campaign.get('startDate').replace(/-/g, '/');
+    var endDate = campaign.get('endDate').replace(/-/g, '/');
+    var startTime = campaign.get('startTime');
+    var endTime = campaign.get('endTime');
+    // 计算星期几
+    var startDay = new Date(Date.parse(startDate)).getDay();
+    var strStartDay = util.parseDay(startDay);
+    var endDay = new Date(Date.parse(endDate)).getDay();
+    var strEndDay = util.parseDay(endDay);
+    if (startDate == endDate) {
+      oneDay = startDate + strStartDay + ' ' + startTime + '-' + endTime;
+    } else {
+      startDateTime =  startDate + strStartDay + ' ' + startTime;
+      endDateTime = endDate + strEndDay + ' ' + endTime;
+    }
+
+    return [oneDay, startDateTime, endDateTime];
   },
     
-    tabClick: function (e) {
-        this.setData({
-            sliderOffset: e.currentTarget.offsetLeft,
-            activeIndex: e.currentTarget.id
-        });
-    }  
+  tabClick: function (e) {
+      this.setData({
+          sliderOffset: e.currentTarget.offsetLeft,
+          activeIndex: e.currentTarget.id
+      });
+  }  
 })
